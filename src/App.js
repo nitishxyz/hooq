32d99149d595e32c04b91d852a3ab41018825c69
Nick <nitnk9@protonmail.com>
2019-01-31 23:23:46 +0530

    trying to fix routing problem

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
+      <Router basename={process.env.PUBLIC_URL}>
+      <div className="App">
       <Route
         path="/hooq/"
         render={props => <Home {...props}/>}
