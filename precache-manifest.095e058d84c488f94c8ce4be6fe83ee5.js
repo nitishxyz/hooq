11b65080bccfa03c9800c5032db57c45587b4ad7
Nick <nitnk9@protonmail.com>
2019-01-31 23:03:04 +0530

    Updates

---
+++ precache-manifest.095e058d84c488f94c8ce4be6fe83ee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaad0560ec8b41d74b88c574f8747869",
+    "url": "/hooq/static/media/Big_buck_bunny_poster_big.aaad0560.jpg"
+  },
+  {
+    "revision": "404d6c4715931510125c",
+    "url": "/hooq/static/js/runtime~main.404d6c47.js"
+  },
+  {
+    "revision": "857fc6360e4539ea3eb0",
+    "url": "/hooq/static/js/main.857fc636.chunk.js"
+  },
+  {
+    "revision": "dc9a1ab25abd12f1d651",
+    "url": "/hooq/static/js/1.dc9a1ab2.chunk.js"
+  },
+  {
+    "revision": "857fc6360e4539ea3eb0",
+    "url": "/hooq/static/css/main.a27e10aa.chunk.css"
+  },
+  {
+    "revision": "91fb1c33f3c4b325579bb4064c31dcf1",
+    "url": "/hooq/index.html"
+  }
+];
